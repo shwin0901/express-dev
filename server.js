93f08285adb54d6265a54d244f28822135ca67f5
zhuxianggui <401815708@qq.com>
2022-06-11 23:43:02 +0800

    Add wepack-dev-middleware server

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require('express')
 const app = express();
 const fs = require('fs')
 const path = require('path');
+const webpack = require('webpack')
+const webpackMiddleWare = require('webpack-dev-middleware')
+const config = require('./webpack.config.js')
+
 const port = '8888'
+const compiler = webpack(config)
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/a', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './a.html'))
-})
+app.use(
+  webpackMiddleWare(compiler, {
+    publicPath: config.output.publicPath
+  })
+)
 
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, './public/index.html'))
+  res.sendFile(path.resolve(__dirname, './dist/index.html'))
 })
 
 
