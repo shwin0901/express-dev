257ea7373ad74a351fd33f2cd2e306896205e542
xianggui <401815708@qq.com>
2022-07-18 23:23:40 +0800

    webpack配置

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // https://github.com/facebook/create-react-app/blob/main/packages/babel-preset-react-app/create.js
+  // babel-preset-react-app
+  presets: ["react-app"],
+  // presets: ["@babel/preset-env"],
+};
