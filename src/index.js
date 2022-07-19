0b0c0f0a20cec0517ecf7e1c6ac353630d3d37ff
xianggui <401815708@qq.com>
2022-07-19 23:05:45 +0800

    feat: 添加输入文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-function add (x,y) {
-  return x*y
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import RouteElement from './routes'
+import "antd/dist/antd.less";
+
+function add(x, y) {
+  return x * y
 }
 
 console.log(add(22, 9))
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+  <BrowserRouter>
+    <RouteElement />
+  </BrowserRouter>
+)
