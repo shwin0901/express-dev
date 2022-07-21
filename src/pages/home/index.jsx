600e6a33a93beefaa6ebb59f4e6f6364ee0c59c8
xianggui <401815708@qq.com>
2022-07-22 00:09:54 +0800

    feat: 添加多行省略‘

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom'
+import './index.less'
 
-function Home(props) {
-  console.log("1", props)
+function Home() {
+  const desc = useRef()
+  const [ellipsis, setEllipsis] = useState(false)
+
+
+  useEffect(() => {
+    console.log("desc", desc.current.offsetHeight)
+
+  }, [])
+
+  const onEllipsis = () => {
+    setEllipsis(!ellipsis)
+  }
+
   return (
     <>
-      <div>Home</div>
+      <div className='container'>
+        <div className='title'>
+          <h3>标题</h3>
+          <a onClick={onEllipsis}>{ellipsis ? '展开' : '收缩'}</a>
+        </div>
+        <p
+          className='container-ellipsis'
+          ref={desc}
+          style={{
+            WebkitLineClamp: ellipsis ? 3 : undefined
+          }}
+        >
+          Webpack 提供了多种接口来自定义编译过程。 接口间存在一些重叠功能，例如配置选项可能通过 CLI 标志启用， 而其他仅存在于单个接口。 以下概念可以帮助你快速入门。
+        </p>
+      </div>
 
     </>
   )
